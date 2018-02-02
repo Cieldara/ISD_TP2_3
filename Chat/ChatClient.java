4ec76a346e53f4dc811371099d0284bed7e75ae5
Cieldara <benjamin.gontard2912@gmail.com>
2018-02-02 18:23:58 +0100

    Debut interface graphique

--- Chat/ChatClient.java
+++ Chat/ChatClient.java
@@ -2,33 +2,71 @@ import java.rmi.*;
 import java.rmi.server.*;
 import java.rmi.registry.*;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.event.*;
 
-public class ChatClient {
-    public static void main(String [] args) {
+public class ChatClient extends Application{
 
+    public void start(Stage stage){
         try {
-          if (args.length < 2) {
+          if (getParameters().getRaw().size() < 2) {
            System.out.println("Usage: java HelloClient <rmiregistry host>");
            return;}
 
-           String host = args[0];
+           String host = getParameters().getRaw().get(0);
 
-           Client client = new Client(args[1]);
+           Client client = new Client(getParameters().getRaw().get(1));
            Accounting_itf a_stub = (Accounting_itf) UnicastRemoteObject.exportObject(client, 0);
 
            Registry registry = LocateRegistry.getRegistry(host);
            Chat_itf chat = (Chat_itf)registry.lookup("ChatService");
 
-           Recup_impl r = new Recup_impl();
-           Recup_itf chat_stub = (Recup_itf) UnicastRemoteObject.exportObject(r, 0);
-           registry.bind("RecupService"+args[1], chat_stub);
 
-           chat.join(client);
+
+           BorderPane root = new BorderPane();
+           ListView displayArea = new ListView();
+           ScrollPane displayContainer = new ScrollPane(displayArea);
+           BorderPane inputPane = new BorderPane();
+
+           TextField messageField = new TextField();
+           Button send = new Button();
+           inputPane.setCenter(messageField);
+           inputPane.setRight(send);
+           root.setCenter(displayContainer);
+           root.setBottom(inputPane);
+
+           stage.setTitle("We-llBehaved Chat");
+           stage.setScene(new Scene(root));
+           stage.show();
+           send.setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                    try{
+                        chat.sendMessage(a_stub,messageField.getText());
+                    }
+                    catch(Exception ex){
+
+                    }
+                    messageField.clear();
+                }
+            });
+
+            Recup_impl r = new Recup_impl(displayArea);
+            Recup_itf chat_stub = (Recup_itf) UnicastRemoteObject.exportObject(r, 0);
+            registry.bind("RecupService"+getParameters().getRaw().get(1), chat_stub);
+
+            chat.join(client);
 
            Scanner scanner = new Scanner(System.in);
            String res;
            boolean fin = false;
-
+           /*
            while(!fin){
 
                String message = scanner.nextLine();
@@ -45,11 +83,15 @@ public class ChatClient {
            }
 
            chat.leave(client);
-           registry.unbind("RecupService"+args[1]);
-           System.exit(0);
+           registry.unbind("RecupService"+getParameters().getRaw().get(1));
+           System.exit(0);*/
 
         }catch (Exception e)  {
             System.err.println("Error on client: " + e);
         }
+    }
+    public static void main(String [] args) {
+        launch(args);
+
     }
 }
--- Chat/Recup_impl.java
+++ Chat/Recup_impl.java
@@ -1,9 +1,18 @@
 import java.rmi.*;
+import javafx.scene.control.ListView;
 
 
 public class Recup_impl implements Recup_itf{
+    private ListView list;
+
+    public Recup_impl(ListView l){
+        this.list = l;
+    }
+
     public void recupMessage(String message)  throws RemoteException{
         System.out.println(message);
+	l.add(message);
+
     }
 
 }
